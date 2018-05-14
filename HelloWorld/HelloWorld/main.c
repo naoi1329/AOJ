ca3d9a6f7636283ea6256232d2f5af8564b3ebde
naoi1329 <x15071xx@aitech.ac.jp>
2018-05-15 02:52:54 +0900

    Hello Worldを追加
    AOJ 1問目を回答

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by 直井翔汰 on 2018/05/14.
+//  Copyright © 2018年 直井翔汰. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
